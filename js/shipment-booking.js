9507ae2ca22f53ae2faca45754c378003cbe4fec
Ash6323 <ashwin6323@gmail.com>
2023-04-08 14:03:01 +0530

    Started js validations

--- js/shipment-booking.js
+++ js/shipment-booking.js
@@ -1,10 +1,45 @@
+function getFormValues() 
+{
+    var letters = /^[A-Za-z]+$/;
+    const bookingName = document.getElementById("booking-name").value;
+    if (bookingName == null || bookingName == "")
+    {  
+        alert("Booking Name cannot be blank");
+        return false;
+    }
+    if (!bookingName.match(letters)) 
+    {
+        alert("Please enter a valid input -> (Booking Name)");
+        return false;
+    }
+
+    const consigneeDropdown = document.getElementById("consignee-dropdown").value;
+    if (consigneeDropdown == null || consigneeDropdown == "")
+    {  
+        alert("Please Select a Consignee");
+        return false;
+    }
+
+    const supplierDropdown = document.getElementById("supplier-dropdown").value;
+    if (supplierDropdown == null || supplierDropdown == "")
+    {  
+        alert("Please Select a Supplier");
+        return false;
+    }
+
+    console.log("Booking Name: "+ bookingName);
+    console.log("Consignee: " + consigneeDropdown);
+    console.log("Supplier: " + supplierDropdown);
+
+}
+
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function myFunction() 
 {
     document.getElementById("myDropdown").classList.toggle("show");
 }
-  
+
 function filterFunction() 
 {
     var input, filter, ul, li, a, i;
